a935f37187243280f673cfc44f4e54ac78423a52
Aayush Niroula <ayushniroula644@gmail.com>
2024-10-30 19:43:07 +0545

    url changed

--- frontend/src/hooks/useGetAllPosts.jsx
+++ frontend/src/hooks/useGetAllPosts.jsx
@@ -8,7 +8,7 @@ const useGetAllPosts=()=>{
     useEffect(()=>{
     const fetchAllPost =async()=>{
         try {
-            const res= await axios.get('http://localhost:8000/api/v1/post/all',{withCredentials:true})
+            const res= await axios.get('https://instagramclone-u8xy.onrender.com/api/v1/post/all',{withCredentials:true})
             if(res.data.success){
               dispatch(setPosts(res.data.post))
         
